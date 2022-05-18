b4c969d86454ae4e63cca904b0ead4dcb93cdaef
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:50:54 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "demos-cmake"
-project_long_description = "Demos for C-make"
-project_year_started = 2019
-project_description = """Demos for C-make"""
+description_short = "Demos for C-make"
