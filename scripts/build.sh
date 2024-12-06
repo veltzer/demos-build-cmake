6c2f5a0ce1753968bd2b30e54003e0aba2998fd5
Mark Veltzer <mark.veltzer@gmail.com>
2024-12-06 02:16:43 +0200

    no commit message given

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,11 @@
+#!/bin/bash -e
 # configure only and generate files in the current folder (not good)
 # cmake .
 # configure only and generate files in the "build" folder, this is the old way
 # cmake -B build
 # configure only and say where the sources are, the new way
 echo "configuring..."
-cmake -B build -S .
+cmake -Werror=dev -B build -S .
 echo "building..."
 cmake --build build
 # run make directly
