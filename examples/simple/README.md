c6c8afecf70ffa6ca3e74ee05ef737dda795c644
Mark Veltzer <mark.veltzer@gmail.com>
2025-03-09 10:53:31 +0200

    new example

---
+++ examples/simple/README.md
@@ -0,0 +1,1 @@
+This is a classic single library with a single binary dependent on it.
---
+++ examples/simple/hello/hello.cc
@@ -0,0 +1,7 @@
+#include <stdlib.h> // for STATUS_SUCCESS
+#include <stdio.h> // for printf(3)
+
+int main(int argc, char** argv, char** envp) {
+	printf("Hello from mybin\n");
+	return EXIT_SUCCESS;
+}
