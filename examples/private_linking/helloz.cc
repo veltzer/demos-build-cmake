bde454c0fa740d4c8080cb95c38c4fd92a5bc43e
Mark Veltzer <mark.veltzer@gmail.com>
2026-01-21 17:33:10 +0200

    no commit message given

---
+++ examples/private_linking/helloz.cc
@@ -0,0 +1,42 @@
+/*
+ * This file is part of the demos-linux package.
+ * Copyright (C) 2011-2023 Mark Veltzer <mark.veltzer@gmail.com>
+ *
+ * demos-linux is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * demos-linux is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with demos-linux. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <zlib.h>	// for compress2(3)
+#include <stdio.h>	// for printf(3)
+#include <string.h>	// for strlen(3)
+#include <stdlib.h>	// for EXIT_SUCCESS
+
+/*
+ * A simple example of using the libz library.
+ *
+ * EXTRA_LINK_FLAGS_AFTER=-lz
+ */
+
+int main(int argc, char** argv, char** envp) {
+	const char *source="Merry had a little lamb";
+	unsigned long sourcelen=strlen(source);
+	unsigned long destlen=100;
+	unsigned char *dest=new unsigned char[destlen];
+	int code=compress2(dest, &destlen, (const unsigned char *)source, sourcelen, Z_DEFAULT_COMPRESSION);
+	if (code!=Z_OK) {
+		printf("compress failed with code [%d]\n", code);
+		return(1);
+	}
+	printf("result is [%s]\n", dest);
+	return EXIT_SUCCESS;
+}
